0a5adf1b8c88f3476ae31655a48add545f18ad1e
zhang0peter <zsd780885029@gmail.com>
2018-07-26 11:29:26 +0800

    更新

--- qdaily-comment.py
+++ qdaily-comment.py
@@ -7,12 +7,11 @@ Created on Wed Jun 13 20:30:20 2018
 
 import matplotlib.pyplot as plt
 import sqlite3
-
 conn = sqlite3.connect(r"qdaily.db")
 url_id = conn.execute("select id from qdaily order by id;").fetchall()
 comment = conn.execute("select comments from qdaily order by id;").fetchall()
-plt.title("id与分享数关系图", fontproperties='SimHei', fontsize=25)
+plt.title("id与评论数关系图", fontproperties='SimHei', fontsize=25)
 plt.xlabel("id", fontproperties='SimHei', fontsize=15)
-plt.ylabel("分享数", fontproperties='SimHei', fontsize=15)
-plt.plot(url_id, comment, 'o')
+plt.ylabel("comment", fontproperties='SimHei', fontsize=15)
+plt.plot(url_id, comment, '-r')
 conn.close()
